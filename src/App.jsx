ac3728d2442f5d79dcf69f50c09191e3bef13730
Dylan Backus <duradylan@gmail.com>
2024-03-19 14:08:49 +0100

    Begin api's

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.coincap.io/v2/assets')
+      .then(function(response) {
+        console.log(response.data.data);
+        setCoins(response.data.data);
+      })
+      .catch(function(error) {
+        console.error('Error', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {coins.map((coin) => <li key={coin.id}>{coin.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios, { isCancel, AxiosError } from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count = 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+useEffect(() => {
+axios.get('https://api.coincap.io/v2/assets')
+.then(function(response) {
+console.log(response.data.data);
+setCoins(response.data.data);
+})});
+return (
+  <>
+  <ul>
+    {coins && coins.map((coin) => <li key={coin.id}>{coin.naam}</li>)}
+  </ul>
+  </>
+) 
+export default App */
--- src/Staafdiagram.jsx
+++ src/Staafdiagram.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import startClock from './clock';
-import axios, {isCancel, AxiosError} from 'axios';
+import axios from 'axios';
 
 const Staafdiagram = () => {
+    const [cashInBank, setCashInBank] = useState(null);
+
     useEffect(() => {
         const intervalId = startClock();
 
@@ -11,47 +13,63 @@ const Staafdiagram = () => {
         };
     }, []);
 
+    useEffect(() => {
+        const exampleData = {
+            cashInBank: "616.5"
+        };
+
+        setCashInBank(exampleData.cashInBank);
+    }, []);
+
     return (
-        <div className="container">
-            <div className="row1">
-                <div className="blok1-top">
-                    <h1 className="blokken-main-text">Cash in bank</h1>
+        <main>
+            <div className="container">
+                <div className="row1">
+                    <div className="blok1-top">
+                        <h1 className="blokken-main-text">Cash in bank</h1>
+                        <div className='cash-in-bank-container'>
+                            <div className="cash-in-bank-dollar">$</div>
+                            <h1 className='cash-in-bank-text'> {cashInBank}</h1>
+                            <h1 className='cash-in-bank-k'>K</h1>
+                            <h1 className='cash-in-bank-current'>Current</h1>
+                        </div>
+                    </div>
+                    <div className="blok2-top">
+                        <h1 className="blokken-main-text">Burn</h1>
+                    </div>
+                    <div className="blok3-top">
+                        <h1 className="blokken-main-text">Expenses</h1>
+                    </div>
                 </div>
-                <div className="blok2-top">
-                    <h1 className="blokken-main-text">Burn</h1>
+                <div className="row2">
+                    <div className="blok1-row2">
+                        <h1 className="blokken-main-text">Solvency</h1>
+                    </div>
+                    <div className="blok2-row2">
+                        <h1 className="blokken-main-text">Deptors</h1>
+                    </div>
+                    <div className="blok3-row2">
+                        <h1 className="blokken-main-text">Deptors (Past 6 months)</h1>
+                    </div>
+                    <div className="blok4-row2">
+                        <h1 className="blokken-main-text">USD X. Rate</h1>
+                    </div>
                 </div>
-                <div className="blok3-top">
-                    <h1 className="blokken-main-text">Expenses</h1>
-                </div>
-            </div>
-            <div className="row2">
-                <div className="blok1-row2">
-                    <h1 className="blokken-main-text">Solvency</h1>
-                </div>
-                <div className="blok2-row2">
-                    <h1 className="blokken-main-text">Deptors</h1>
-                </div>
-                <div className="blok3-row2">
-                    <h1 className="blokken-main-text">Deptors (Past 6 months)</h1>
-                </div>
-                <div className="blok4-row2">
-                    <h1 className="blokken-main-text">USD X. Rate</h1>
-                </div>
-            </div>
-            <div className="promo-onder">
-                <div className="cashflow-text-container">
-                    <img className="mijn-logo" src="./media/Garfield PFP.jpg" alt="Logo"></img>
-                    <h1 className="cashflow-text-promo">Cashflow Dashboard</h1>
-                    <h1 className="powered-by-text-promo">Powered by </h1>
-                    <a href="https://dylanbackus.github.io/" className="dylan-backus-text-promo">Dylan Backus</a>
-                </div>
-                <div className="klok-bottom-container">
-                    <div id="clock">
-                        <h1 id="date-time" className="klok-bottom"></h1>
+                <div className="promo-onder">
+                    <div className="cashflow-text-container">
+                        <img className="mijn-logo" src="./media/Garfield PFP.jpg" alt="Logo"></img>
+                        <h1 className="cashflow-text-promo">Cashflow Dashboard</h1>
+                        <h1 className="powered-by-text-promo">Powered by </h1>
+                        <a href="https://github.com/DylanBackus" className="dylan-backus-text-promo">Dylan Backus</a>
+                    </div>
+                    <div className="klok-bottom-container">
+                        <div id="clock">
+                            <h1 id="date-time" className="klok-bottom"></h1>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     );
 };
 
